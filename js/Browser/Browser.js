13e8492587b1914b3fc848f8b8ef6f6fab93bda7
Edy Segura <edysegura@gmail.com>
2017-01-12 23:09:31 -0200

    First commit as baseline

---
+++ js/Browser/Browser.js
@@ -0,0 +1,164 @@
+﻿/**
+ *
+ * Objeto Literal Browser. Documentacao completa disponivel em: 
+ * http://code.google.com/p/jscomponentes/wiki/Browser
+ *
+ * Browser.js
+ * http://jscomponentes.googlecode.com/svn/trunk/Browser/js/Browser/Browser.js
+ * @author: Edy Segura - edy@segura.pro.br
+ *
+ */
+var Browser = {
+	
+	//atributos publicos
+	name    : navigator.appName,
+	ua      : navigator.userAgent.toLowerCase(),
+	version : parseFloat(navigator.appVersion.substr(21)) || parseFloat(navigator.appVersion),
+	java    : navigator.javaEnabled(),
+	win     : navigator.platform == 'Win32',
+	dom     : (document.createElement && document.getElementById) ? true : false,
+	
+	//search engine's
+	isAddSearchProvider : (window.external && ("AddSearchProvider" in window.external)) ? true : false,
+	isAddSearchEngine   : (window.sidebar && ("addSearchEngine" in window.sidebar)) ? true : false,
+	
+	
+	addBookmark: function(location, title) {
+		location = (location) ? location : document.location.href;
+		title    = (title) ? title : document.title;
+		
+		if(Browser.isIE) {
+			window.external.AddFavorite(location, title);
+		}
+		else if(typeof window.sidebar == "object" && typeof window.sidebar.addPanel == "function") {
+      window.sidebar.addPanel(title, location, "");
+		}
+		else {
+			if(confirm("Seu navegador não suporta a extensão sidebar. Você quer fazer o upgrade agora?")) {
+				document.location.href = "http://getfirefox.com/";
+			}
+		}
+	},
+	
+	
+	installSearchEngine: function() {
+		var urlSearch = arguments[0];
+		var urlIcon   = arguments[1];
+		var title     = arguments[2];
+		var msgError  = "Your browser not supported search engine.";
+		
+		if(arguments.length == 1) {
+			// Firefox 2 and IE 7, OpenSearch
+			if(window.external && ("AddSearchProvider" in window.external)) {
+				window.external.AddSearchProvider(urlSearch);
+			}
+			else {
+				alert(msgError);
+			}
+		}
+		else if(arguments.length == 3) {
+			//Firefox <= 1.5, Sherlock
+			if(window.sidebar && ("addSearchEngine" in window.sidebar)) {
+				window.sidebar.addSearchEngine(urlSearch, urlIcon, title, "");
+			}
+			else {
+				alert(msgError);
+			}
+		}
+		
+	},
+	
+	
+	getPageSize: function() {
+		var scrollX, scrollY, pageSize;
+		var viewWidth, viewHeight;
+		var pageWidth, pageHeight;
+		
+		if(window.innerHeight && window.scrollMaxY) {	
+			scrollX = document.body.scrollWidth;
+			scrollY = window.innerHeight + window.scrollMaxY;
+		} 
+		else if(document.body.scrollHeight > document.body.offsetHeight){
+			scrollX = document.body.scrollWidth;
+			scrollY = document.body.scrollHeight;
+		} 
+		else { 
+			scrollX = document.body.offsetWidth;
+			scrollY = document.body.offsetHeight;
+		}
+		
+		if(self.innerHeight) {
+			viewWidth  = self.innerWidth;
+			viewHeight = self.innerHeight;
+		} 
+		else if(document.documentElement && document.documentElement.clientHeight) { 
+			viewWidth  = document.documentElement.clientWidth;
+			viewHeight = document.documentElement.clientHeight;
+		} 
+		else if(document.body) {
+			viewWidth  = document.body.clientWidth;
+			viewHeight = document.body.clientHeight;
+		}
+		
+		//coordenada X
+		if(scrollX < viewWidth){	
+			pageWidth = viewWidth;
+		} 
+		else {
+			pageWidth = scrollX;
+		}
+		
+		//coordenada Y
+		if(scrollY < viewHeight){
+			pageHeight = viewHeight;
+		} 
+		else { 
+			pageHeight = scrollY;
+		}
+		
+		//retorna objeto
+		return pageSize = {
+			pageWidth  : pageWidth,
+			pageHeight : pageHeight,
+			viewWidth  : viewWidth,
+			viewHeight : viewHeight
+		};
+		
+	},
+
+	
+	getScroll: function() {
+		var scrollX = 0, scrollY = 0, pageScroll;
+		
+		if(self.pageYOffset) {
+			scrollX = self.pageXOffset;
+			scrollY = self.pageYOffset;
+		}
+		else if(document.documentElement && document.documentElement.scrollTop) {
+			scrollX = document.documentElement.scrollLeft;
+			scrollY = document.documentElement.scrollTop;
+		}
+		else if(document.body) {
+			scrollX = document.body.scrollLeft;
+			scrollY = document.body.scrollTop;
+		}
+		
+		//retorna objeto
+		return pageScroll = {
+			x : scrollX,
+			y : scrollY
+		};
+		
+	}
+	
+};
+
+//atributos publicos do objeto literal Browser
+Browser.isSafari  = Browser.ua.indexOf("safari") >= 0;
+Browser.isOpera   = Browser.ua.indexOf("opera")  >= 0;
+Browser.isMac     = Browser.ua.indexOf("mac")    >= 0;
+Browser.isGecko   = Browser.ua.indexOf("gecko")  >= 0;
+Browser.isFirefox = !Browser.isOpera && !Browser.isSafari && (Browser.name == "Netscape");
+Browser.isIE      = !Browser.isOpera && (Browser.name == "Microsoft Internet Explorer");
+Browser.isIE6     = Browser.isIE && Browser.ua.indexOf("msie 6.0") >= 0;
+Browser.isIE7     = Browser.isIE && Browser.ua.indexOf("msie 7.0") >= 0;
---
+++ js/Index.js
@@ -0,0 +1,113 @@
+﻿/**
+ *
+ * Objeto Literal Index para uso do componente Browser.js
+ * @author: Edy Segura - edy@segura.pro.br
+ *
+ */
+var Index = {
+	
+	init: function() {
+		Index.buildTable();
+		Index.createButtons();
+		Index.setWinResize();
+	},
+	
+	
+	setWinResize: function() {
+		Index.getPageSize();
+		Index.getPageScroll();
+		window.onresize = Index.getPageSize;
+		window.onscroll = Index.getPageScroll;
+	},
+	
+	
+	createButtons: function() {
+		var button1 = document.createElement('button'),
+		    button2 = document.createElement('button'),
+		    button3 = document.createElement('button');
+		
+		button1.onclick = function() {
+			if(Browser && Browser.addBookmark) {
+				Browser.addBookmark("http://edysegura.com", "Edy Segura");
+			}
+		}
+		
+		button2.onclick = function() {
+			if(Browser && Browser.isAddSearchProvider) {
+				Browser.installSearchEngine("http://files.edysegura.com/xml/opensearch/edysegura.com.xml");
+			}
+		}
+		
+		button3.onclick = function() {
+			if(Browser && Browser.isAddSearchEngine) {
+				Browser.installSearchEngine("http://files.edysegura.com/xml/sherlock/edysegura.com.src", 
+                                    "http://us.i1.yimg.com/us.yimg.com/i/yg/img/logo/favicon.ico",  
+                                    "Edy Search");
+			}
+		}
+		
+		button1.appendChild(document.createTextNode('Adicionar ao favoritos'));
+		document.body.appendChild(button1);
+		
+		if(Browser.isAddSearchProvider) {
+			button2.appendChild(document.createTextNode('Adicionar search engine (OpenSearch)'));
+			document.body.appendChild(button2);
+		}
+		
+		if(Browser.isAddSearchProvider) {
+			button3.appendChild(document.createTextNode('Adicionar search engine (Sherlock)'));
+			document.body.appendChild(button3);
+		}
+		
+	},
+	
+	
+	buildTable: function() {
+		var table = document.getElementById('properties'),
+		    tbody = table.appendChild(document.createElement('tbody')),
+		    pattern = /addBookmark|installSearchEngine|getPageSize|getScroll/,
+				row, cellProperty, cellValue;
+		
+		//percorrendo o objeto Browser
+		for(var property in Browser) {
+			if(pattern.test(property)) continue;
+			
+			row          = tbody.insertRow(tbody.rows.length);
+			cellProperty = row.insertCell(row.cells.length);
+			cellValue    = row.insertCell(row.cells.length);
+			
+			cellProperty.innerHTML = property;
+			cellValue.innerHTML    = Browser[property];
+		}
+		
+		table.removeChild(table.tBodies[0]);
+	},
+	
+	
+	getPageSize: function() {
+		var pageSize   = Browser.getPageSize(),
+		    pageWidth  = document.getElementById('pageWidth'),
+		    pageHeight = document.getElementById('pageHeight'),
+		    viewWidth  = document.getElementById('viewWidth'),
+		    viewHeight = document.getElementById('viewHeight');
+		
+		pageWidth.innerHTML  = pageSize.pageWidth;
+		pageHeight.innerHTML = pageSize.pageHeight;
+		viewWidth.innerHTML  = pageSize.viewWidth;
+		viewHeight.innerHTML = pageSize.viewHeight;
+	},
+	
+	
+	getPageScroll: function() {
+		var pageScroll = Browser.getScroll(),
+		    scrollTop  = document.getElementById("scrollTop"),
+		    scrollLeft = document.getElementById("scrollLeft");
+		
+		scrollLeft.innerHTML = pageScroll.x;
+		scrollTop.innerHTML  = pageScroll.y;
+	}
+	
+};
+
+//inicializacao
+window.onload = Index.init;
